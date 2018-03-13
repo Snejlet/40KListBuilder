be3ab552f4d78f3a0bdb71efce9294eb9127bcb1
Snejlet <seanp_james@hotmail.com>
2018-03-13 18:59:50 +0000

    ejs files cleaned up significantly.

--- 40K List Builder/dao/unitDAO.js
+++ 40K List Builder/dao/unitDAO.js
@@ -9,6 +9,10 @@ var unitDAO = {
     getAllBySubFaction:function(subFaction, callback) {
         var sql = "select * from "+this.tableName+" where subfaction = ?;";
         daoHelper.callSql(sql, [subFaction], "getAllBySubFaction", callback);
+    },
+    getUnitById:function(id, callback) {
+        var sql = "select * from "+this.tableName+" where id = ?;";
+        daoHelper.callSql(sql, [id], "getUnitById", callback);
     }
 };
 
